cf6e716d21eea0083f1435ce2bba2d2566efba0b
Noah Devine <Noah.Devine@keyin.com>
2025-04-07 15:17:36 -0230

    Created users, admin, trainer, and member classes
    - Created parent User.java class along with a basic implmentation of its subclasses trainer, admin, and member.

--- src/main/java/org/keyin/user/User.java
+++ src/main/java/org/keyin/user/User.java
@@ -1,8 +1,29 @@
 package org.keyin.user;
-
-//*
-// This is the parent class for all users, There are 3 types of users: Trainer, Member, and Admin
-//
-// *//
 public class User {
+    private int user_id;
+    private String username;
+    private String password;
+    private String address;
+    private String email;
+    private String phone_number;
+    private String role;
+
+    public User (int user_id, String username, String password, String address, String email, String phone_number, String role) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.role = role;
+    }
+
+    public User (String username, String password, String address, String email, String phone_number, String role) {
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.role = role;
+    }
 }
--- src/main/java/org/keyin/user/childclasses/Member.java
+++ src/main/java/org/keyin/user/childclasses/Member.java
@@ -3,4 +3,7 @@ package org.keyin.user.childclasses;
 import org.keyin.user.User;
 
 public class Member extends User {
+    public Member(int user_id, String username, String password, String address, String email, String phone_number) {
+        super(user_id, username, password, address, email, phone_number, "MEMBER");
+    }
 }
